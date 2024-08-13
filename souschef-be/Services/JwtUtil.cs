f2eef8bab6cbab3e494f5c19ceec7f460280716f
Zac A <me@zaca.dev>
2024-08-12 18:51:54 -0700

    Working on JWT implementation, untested token generator written.

--- souschef-be/Services/JwtUtil.cs
+++ souschef-be/Services/JwtUtil.cs
@@ -1,30 +1,49 @@
 using System.IdentityModel.Tokens.Jwt;
-using FastEndpoints.Security;
+using System.Security.Claims;
+using System.Text;
 using souschef_core.Model;
 using souschef_core.Model.DTO;
 using souschef_core.Services;
 using BCrypt.Net;
+using Microsoft.IdentityModel.Tokens;
 
 namespace souschef_be.Services;
 
-public class JwtUtil(UserService userSvc) : IJwtUtil
+public class JwtUtil(UserService userSvc, IConfiguration config) : IJwtUtil
 {
     /// <summary>
     /// Generates a new JSON Web Token for the given user. Tokens are valid for 30 days.
     /// </summary>
     /// <param name="user">The user to provide a token for.</param>
     /// <param name="ct">Cancellation token to cancel this request.</param>
+    /// <exception cref="ArgumentNullException"></exception>
     /// <returns>A JSON Web Token for the given user.</returns>
     public string GenerateToken(User user, CancellationToken ct)
     {
-        return JwtBearer.CreateToken(
-            t =>
-            {
-                t.ExpireAt = DateTime.UtcNow.AddDays(30);
-                t.User.Roles.Add("User");
-                t.User.Claims.Add(("Username", user.Username!));
-                t.User
-            });
+        var tokinator = new JwtSecurityTokenHandler();
+        var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY") ??
+                                                                   throw new InvalidOperationException()));
+        var signature = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub,
+                user.UserId.ToString() ?? throw new ArgumentNullException(nameof(user.UserId))),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, user.Username ?? throw new ArgumentNullException(nameof(user.Username)))
+        };
+
+        var desc = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(Convert.ToDouble(config["JWT:ExpiryDays"])),
+            IssuedAt = DateTime.UtcNow,
+            Issuer = config["JWT:Issuer"],
+            Audience = config["JWT:Audience"],
+            SigningCredentials = signature
+        };
+
+        var token = tokinator.CreateToken(desc);
+        return tokinator.WriteToken(token);
     }
 
     /// <summary>
@@ -35,7 +54,7 @@ public class JwtUtil(UserService userSvc) : IJwtUtil
     /// <returns>User ID from token if valid or null if token is invalid.</returns>
     public async Task<int?> ValidateToken(string token, string username)
     {
-        var user = userSvc.QueryByUsername(username);
+        throw new NotImplementedException();
+        return null;
     }
-    
 }
