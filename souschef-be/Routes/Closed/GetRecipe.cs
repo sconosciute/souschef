6271d5210ad0ed6c79f6c8d68ca8b0f2205fc4bb
apholmes14 <andrew.p.holmes14@gmail.com>
2024-08-13 19:37:26 -0700

    updating getRecipe

--- souschef-be/Routes/Closed/GetRecipe.cs
+++ souschef-be/Routes/Closed/GetRecipe.cs
@@ -7,5 +7,18 @@ namespace souschef_be.Routes.Closed;
 
 public class GetRecipe(SkinnyRecipeService recipeSvc) : Endpoint<SkinnyRecipe>
 {
-    
+    public override void Configure()
+    {
+        Get("/recipe");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(SkinnyRecipe req, CancellationToken ct)
+    {
+        var recipes = await recipeSvc.GetRecipeInfoBasic(req.id);
+        if (recipes is not null)
+        {
+            
+        }
+    }
 }
