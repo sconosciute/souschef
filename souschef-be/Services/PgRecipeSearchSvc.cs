bab1cf14389b54a445d20c1101dd02a4af186ad6
Zac A <me@zaca.dev>
2024-08-14 11:21:22 -0700

    Search tested and working

--- souschef-be/Services/PgRecipeSearchSvc.cs
+++ souschef-be/Services/PgRecipeSearchSvc.cs
@@ -10,7 +10,7 @@ public class PgRecipeSearchSvc(DbContext db) : ISearchSvc
     public async Task<List<ThinRecipe>> SearchByName(string name, int page, int pageSize)
     {
         return await db.Set<Recipe>()
-            .Where(r => r.Name != null && r.Name.Contains(name))
+            .Where(r => r.Name != null && r.Name.ToLower().Contains(name.ToLower()))
             .Select(r => new ThinRecipe
                 { id = r.RecipeId, name = r.Name, description = r.Description, tags = r.GetTagEntities().Result })
             .Skip((page - 1) * pageSize)
@@ -21,7 +21,7 @@ public class PgRecipeSearchSvc(DbContext db) : ISearchSvc
     public async Task<List<ThinRecipe>> SearchByTags(List<long> tagIds, int page, int pageSize)
     {
         return await db.Set<Recipe>()
-            .Where(r => r.Tags != null && r.Tags.Any(tagIds.Contains))
+            .Where(r => r.Tags != null && tagIds.All(t => r.Tags.Contains(t)))
             .Select(r => new ThinRecipe
             {
                 id = r.RecipeId, description = r.Description, name = r.Name, tags = r.GetTagEntities().Result
