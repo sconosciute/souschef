15d6b3f161915f0fd90f717f03706985992d3c1f
Zac A <me@zaca.dev>
2024-08-14 10:30:58 -0700

    Implemented QueryParams record to hold global query string info.

---
+++ souschef-core/Model/Definitions/QueryParams.cs
@@ -0,0 +1,8 @@
+namespace souschef_core.Model;
+
+public record QueryParams
+{
+    public const string Query = "q";
+    public const string Page = "page";
+    public const string PageSize = "pageSize";
+}
