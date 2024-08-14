151d71117c6944fc1a9db1c2008689f774fa5b70
apholmes14 <andrew.p.holmes14@gmail.com>
2024-08-13 19:32:11 -0700

    Working on getting a skinny recipe service to display basic recipe information

---
+++ souschef-be/Routes/Closed/GetRecipe.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+using souschef_be.Services;
+using souschef_core.Model;
+using souschef_core.Model.DTO;
+
+namespace souschef_be.Routes.Closed;
+
+public class GetRecipe(SkinnyRecipeService recipeSvc) : Endpoint<SkinnyRecipe>
+{
+    
+}
